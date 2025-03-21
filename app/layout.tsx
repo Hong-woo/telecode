21536e80184b01fb4e4379d6c18e5c0b956f867c
Hong-woo <wyhong1@gmail.com>
2025-03-21 15:25:12 +0900

    Fix layout.tsx duplicate imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "텔레코드(Tel Record) - 사무실 전화 통화 녹음 솔루션",
   description: "텔레코드는 사무실의 인터넷 전화 통화를 녹음해서 들을 수 있는 솔루션입니다.",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
